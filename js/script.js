8d9908d9e0540256d11fe6d20a4468ad2051d479
sergkrasivskyi <sergii.krasivskyi@gmail.com>
2022-07-06 19:10:08 +0300

    block to link

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function getResponsiveMenu() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
